0b31804df7cc22040c7eb2af9f133654f2d64369
Bigaisha <shalabayeva.bigaisha@gmail.com>
2022-01-18 22:08:08 +0600

    QA homework

---
+++ src/test/java/tests/PracticeForm.java
@@ -0,0 +1,63 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class PracticeForm {
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
+    @Test
+    void practice() {
+
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        $("[alt=\"adplus-dvertising\"]").click();
+
+        $("#firstName").setValue("Bigaisha");
+        $("#lastName").setValue("Shalabayeva");
+        $("#userEmail").setValue("bigaisha@gmail.com");
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#userNumber").setValue("7023998900");
+        $("#currentAddress").setValue("Samal-1-19-3");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("November");
+        $(".react-datepicker__year-select").selectOption("1991");
+        $("[aria-label$='November 5th, 1991']").click();
+
+        $("#subjectsInput").setValue("Eng");
+        $("#subjectsWrapper").$(byText("English")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+
+        $("#uploadPicture").uploadFromClasspath("img/1.png");
+
+        $("#state").scrollTo().click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+
+        $("#submit").click();
+
+        sleep(5000);
+
+        $(".table").shouldHave(
+                text("Bigaisha Shalabayeva"), text("bigaisha@gmail.com"),
+                text("Female"), text("7023998900"),
+                text("05 November,1991"), text("English"),
+                text("Reading"), text("1.png"),
+                text("Samal-1-19-3"), text("NCR Delhi"),
+                text("7023998900"), text("7023998900")
+        );
+    }
+}
+
+
+
---
+++ src/test/java/tests/TextBoxTests.java
@@ -0,0 +1,29 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+
+    @Test
+
+    void successTest() {
+        open( "https://demoqa.com/text-box");
+
+        $("#userName").setValue("Bigaisha");
+        $("#userEmail").setValue("bigaisha@gmail.com");
+        $("#currentAddress").setValue("Almaty");
+        $("#permanentAddress").setValue("Alma-Ata");
+        $("#submit").scrollTo().click();
+
+        $("#output").shouldBe(visible);
+        $("#name").shouldHave(text("Bigaisha"));
+        $("#email").shouldHave(text("bigaisha@gmail.com"));
+        $("#output #currentAddress").shouldHave(text("Almaty"));
+        $("#output").$("#permanentAddress").shouldHave(text("Alma-Ata"));
+    }
+}
