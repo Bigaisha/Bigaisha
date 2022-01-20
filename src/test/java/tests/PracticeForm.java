db5edbe3b84ba11738c3fd13addc5a102ef2de16
Bigaisha <shalabayeva.bigaisha@gmail.com>
2022-01-20 20:59:52 +0600

    QA homework

--- src/test/java/tests/PracticeForm.java
+++ src/test/java/tests/PracticeForm.java
@@ -24,6 +24,12 @@ public class PracticeForm {
         $("#firstName").setValue("Bigaisha");
         $("#lastName").setValue("Shalabayeva");
         $("#userEmail").setValue("bigaisha@gmail.com");
+
+//        $("#gender-radio-3").click(); // NOT WORKING
+//        $("#gender-radio-3").doubleClick(); // BUT WHY?
+//        $("#gender-radio-3").parent().click();
+//        $("label[for=gender-radio-3]").click();
+//        $(byText("Other")).click(); // BAD PRACTICE
         $("#genterWrapper").$(byText("Female")).click();
         $("#userNumber").setValue("7023998900");
         $("#currentAddress").setValue("Samal-1-19-3");
@@ -31,11 +37,33 @@ public class PracticeForm {
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("November");
         $(".react-datepicker__year-select").selectOption("1991");
+
+//        $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
+//        $$(".react-datepicker__day--030")
+//                .filter(not(cssClass(".react-datepicker__day--outside-month")))
+//                .first() // or .get(0)
+//                .click();
+//        $("[aria-label=Choose Wednesday, July 30th, 2008]").click(); // NOT WORKING
+//        $("[aria-label=\"Choose Wednesday, July 30th, 2008\"]").click();
+//        $("[aria-label='Choose Wednesday, July 30th, 2008']").click();
         $("[aria-label$='November 5th, 1991']").click();
+
+        //        $x("//*[contains(@aria-label, \"July 30th, 2008\")]").click();
+
+// <div class="react-datepicker__day--030 react-datepicker__day--outside-month"  aria-label="Choose Monday, June 30th, 2008">30</div>
+// <div class="react-datepicker__day--030"                                       aria-label="Choose Wednesday, July 30th, 2008">30</div>
+
 
         $("#subjectsInput").setValue("Eng");
         $("#subjectsWrapper").$(byText("English")).click();
+//        $("#subjectsInput").setValue("M"); // todo Why not working
+//        $("#subjectsWrapper").$(byText("Math")).click();
+
         $("#hobbiesWrapper").$(byText("Reading")).click();
+//        $("#uploadPicture").uploadFile(new File("src/test/resources/img/1.png"));
+//        File someFile = new File("src/test/resources/img/1.png");
+//        $("#uploadPicture").uploadFile(someFile);
+
 
         $("#uploadPicture").uploadFromClasspath("img/1.png");
 
